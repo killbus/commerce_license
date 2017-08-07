c1bb435f1c245cbd6424ce8353c4383dd98ddbaf
joachim <joachim@107701.no-reply.drupal.org>
2017-08-07 15:17:15 +0100

    Issue #2886820 by joachim, chrisrockwell: Added configuration form, bundle fields, and license sync to Role license type plugin.

--- src/Plugin/Commerce/LicenseType/Role.php
+++ src/Plugin/Commerce/LicenseType/Role.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\commerce_license\Plugin\Commerce\LicenseType;
 
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\RoleInterface;
 use Drupal\commerce\BundleFieldDefinition;
 use Drupal\commerce_license\Entity\LicenseInterface;
 
@@ -29,14 +31,77 @@ class Role extends Base {
    * {@inheritdoc}
    */
   public function licenseActivated(LicenseInterface $license) {
-    // TODO.
+    // Get the role ID that this license grants.
+    $role_id = $license->license_role->first()->target_id;
+
+    // Get the owner of the license and grant them the role.
+    $owner = $license->getOwner();
+    $owner->addRole($role_id);
+
+    $owner->save();
+
+    // TODO: Log this, as it's something admins should see?
   }
 
   /**
    * {@inheritdoc}
    */
   public function licenseDeactivated(LicenseInterface $license) {
-    // TODO
+    // Get the role ID that this license grants.
+    $role_id = $license->license_role->first()->target_id;
+
+    // Get the owner of the license and remove that role.
+    $owner = $license->getOwner();
+    $owner->removeRole($role_id);
+
+    $owner->save();
+
+    // TODO: Log this, as it's something admins should see?
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $roles = \Drupal::service('entity_type.manager')->getStorage('user_role')->loadMultiple();
+
+    // Skip the built-in roles.
+    unset($roles[RoleInterface::ANONYMOUS_ID]);
+    unset($roles[RoleInterface::AUTHENTICATED_ID]);
+
+    // Remove the admin role if it exists.
+    // TODO: consider removing any role that has is_admin set.
+    unset($roles['administrator']);
+
+    $options = [];
+    foreach ($roles as $rid => $role) {
+      $options[$rid] = $role->label();
+    }
+
+    $form['license_role'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Licensed role'),
+      '#options' => $options,
+      '#default_value' => isset($this->configuration['license_role']) ? $this->configuration['license_role'] : '',
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValue($form['#parents']);
+
+    $this->configuration['license_role'] = $values['license_role'];
   }
 
   /**
@@ -45,7 +110,12 @@ class Role extends Base {
   public function buildFieldDefinitions() {
     $fields = parent::buildFieldDefinitions();
 
-    // @TODO: Role reference field? Get this from the product?
+    $fields['license_role'] = BundleFieldDefinition::create('entity_reference')
+      ->setLabel(t('Roles'))
+      ->setDescription(t('The roles this product grants access to.'))
+      ->setCardinality(1)
+      ->setRequired(TRUE)
+      ->setSetting('target_type', 'user_role');
 
     return $fields;
   }
