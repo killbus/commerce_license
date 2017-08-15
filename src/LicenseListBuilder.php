34c13c3032669fc7d84581e775690d031b5df455
florenttorregrosa <florenttorregrosa@2388214.no-reply.drupal.org>
2017-08-15 10:39:03 +0100

    Issue #2900433 by Grimreaper: Removed name column and added license type in list builder.

--- src/LicenseListBuilder.php
+++ src/LicenseListBuilder.php
@@ -4,8 +4,9 @@ namespace Drupal\commerce_license;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
-use Drupal\Core\Url;
-use Drupal\Core\Link;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a class to build a listing of License entities.
@@ -15,11 +16,44 @@ use Drupal\Core\Link;
 class LicenseListBuilder extends EntityListBuilder {
 
   /**
+   * The license types definitions.
+   *
+   * @var array
+   */
+  protected $licenseTypes;
+
+  /**
+   * Constructs a new LicenseListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\commerce_license\LicenseTypeManager $license_type_manager
+   *   The license type manager service.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, LicenseTypeManager $license_type_manager) {
+    parent::__construct($entity_type, $storage);
+    $this->licenseTypes = $license_type_manager->getDefinitions();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('plugin.manager.commerce_license_type')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildHeader() {
     $header['id'] = $this->t('License ID');
-    $header['name'] = $this->t('Name');
+    $header['type'] = $this->t('Type');
     return $header + parent::buildHeader();
   }
 
@@ -29,14 +63,7 @@ class LicenseListBuilder extends EntityListBuilder {
   public function buildRow(EntityInterface $entity) {
     /* @var $entity \Drupal\commerce_license\Entity\License */
     $row['id'] = $entity->id();
-    $row['name'] = Link::fromTextAndUrl(
-      $entity->label(),
-      new Url(
-        'entity.commerce_license.edit_form', array(
-          'commerce_license' => $entity->id(),
-        )
-      )
-    );
+    $row['type'] = $this->licenseTypes[$entity->bundle()]['label'];
     return $row + parent::buildRow($entity);
   }
 
