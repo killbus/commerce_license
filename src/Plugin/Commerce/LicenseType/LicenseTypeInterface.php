cb30a45923a1ff7b3918cc030724362884128f91
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-09-21 07:58:31 +0100

    Issue #2906399: Added docs to LicenseTypeInterface explaining how license type plugins work.

--- src/Plugin/Commerce/LicenseType/LicenseTypeInterface.php
+++ src/Plugin/Commerce/LicenseType/LicenseTypeInterface.php
@@ -9,6 +9,31 @@ use Drupal\commerce_license\Entity\LicenseInterface;
 
 /**
  * Defines the interface for license types.
+ *
+ * License type plugins provide the functionality for what the license grants
+ * access to.
+ *
+ * They participate in different ways at various points of the license's
+ * lifecycle:
+ * - When a product is created that sells a license, the license type
+ *   plugin's configuration represents the details of the license to be sold.
+ *   This is stored as a configured plugin on a field on the product variation
+ *   (this field is provided by our commerce_license entity trait on product
+ *   variation types).
+ * - When a product is purchased, the configured plugin on the product
+ *   variation is used to set field values on the newly created license entity.
+ *   In effect, the configured plugin is used as a template for the new license.
+ *   These fields are provided by the license type plugin behaving as a bundle
+ *   plugin. Accordingly, the plugin configuration defined in
+ *   defaultConfiguration() and the bundle fields defined in
+ *   buildFieldDefinitions() should usually match. The license has a reference
+ *   to the plugin type in its bundle field, but when a license obtains its
+ *   plugin to work with, it is no longer configured as this would duplicate the
+ *   fields (which we use rather than the plugin configuration so they can be
+ *   queried for).
+ * - When a license is granted or revoked, the plugin's grantLicense()
+ *   and revokeLicense() methods are responsible for making changes to the
+ *   system, such as adding or removing a role on the license's owner.
  */
 interface LicenseTypeInterface extends BundlePluginInterface, ConfigurablePluginInterface, PluginFormInterface {
 
